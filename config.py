62cb5e6b15449fddcf61b5ae786db75add97c7a9
Chien Nguyen <chienqan@gmail.com>
2025-03-01 23:43:56 +0700

    refactor: remove size map

--- config.py
+++ config.py
@@ -10,12 +10,3 @@ MODEL_NAME = "ZhengPeng7/BiRefNet"
 
 # Kích thước đầu vào cho model (resize ảnh thành 1024x1024 trước khi đưa vào model)
 MODEL_INPUT_SIZE = (1024, 1024)
-
-# Các tham số mapping cho kích thước output (đơn giản minh họa)
-SIZE_MAP = {
-    "preview": (625, 400),
-    "small": (625, 400),
-    "regular": (625, 400),
-    "medium": (1280, 720),
-    "hd": (1920, 1080)
-}
