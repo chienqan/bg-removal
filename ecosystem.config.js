a040bc2c92bccbc847dd2cd66f4378856fe5995b
Chien Nguyen <chienqan@gmail.com>
2025-03-30 13:20:13 +0700

    fix: update Gunicorn arguments in ecosystem config to remove unnecessary capture-output flag

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,15 +2,14 @@ module.exports = {
   apps: [{
     name: 'bg-remover',
     script: 'gunicorn',
-    args: '--bind 0.0.0.0:5000 --log-level=info --access-logfile=./logs/gunicorn_access.log --error-logfile=./logs/gunicorn_error.log --workers=2 app:app',
+    args: '--bind 0.0.0.0:5000 --log-level=info --access-logfile=./logs/gunicorn_access.log --error-logfile=./logs/gunicorn_error.log app:app',
     interpreter: './venv/bin/python3',
     cwd: __dirname,
     env: {
       FLASK_ENV: 'production',
       LOG_LEVEL: 'INFO',
       HOST: '0.0.0.0',
-      PORT: '5000',
-      GUNICORN_CMD_ARGS: "--capture-output --enable-stdio-inheritance"
+      PORT: '5000'
     },
     out_file: './logs/out.log',
     error_file: './logs/error.log',
